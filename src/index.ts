2e40f33ae0c6f4d204ff42b6ab3c1bb4df33b615
Sairammargam <sairam.margam2004@gmail.com>
2025-04-25 10:45:34 +0530

    added initial

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import { PrismaClient } from "./generated/prisma"
+const app = express()
+const prismaClient =new  PrismaClient()
+
+app.get("/", async (req,res)=>{
+    const data = prismaClient.user.findMany();
+    res.json({
+         data 
+    })
+})
+app.post("/",async (req,res)=>{
+    await prismaClient.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+    res.json({
+        "message":"post endpoint"
+    })
+})
+ app.listen(3000)
